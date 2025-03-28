1534de3d968a789f3baa354287fd25486530541d
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-03-29 01:05:35 +0600

    blog-model, user-model created

--- blog/model/blog.model.js
+++ blog/model/blog.model.js
@@ -1,3 +1,27 @@
 import mongoose from "mongoose";
 
-const blogSchema = mongoose.schema({});
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please provide a title"],
+      trim: true,
+      maxLength: [100, "Title should not be more than 100 characters"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide a description"],
+      trim: true,
+      maxLength: [1000, "Description should not be more than 1000 characters"],
+    },
+    image: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.Model("Blog", blogSchema);
+export default Blog;
--- blog/model/user.model.js
+++ blog/model/user.model.js
@@ -1,11 +1,69 @@
-import mongoose from mongoose
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
-const userSchema=new mongoose.schema({
-    userName :{},
-    email:{},
-    password:{}
-})
+//define user schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+      maxLength: [25, "Name should not be more than 25 characters"],
+      validate: {
+        validator: (value) => {
+          return validator.isAlpha(value, "en-US", { ignore: " " });
+        },
+        message: "Name should only contain alphabets",
+      },
+    },
 
-const User= mongoose.model("users",userSchema);
+    email: {
+      type: String,
+      required: [true, "Please provide a email"],
+      unique: true,
+      trim: true,
+      maxLength: [100, "Email should not be more than 100 characters"],
+      validate: {
+        validator: (value) => {
+          return validator.isEmail(value);
+        },
+        message: "Please provide a valid email",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minLength: [6, "Password should be greater than 6 characters"],
+      select: false,
+    },
+    blogs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Blog",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
+//hash password before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//compare password with hashed password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//check if email is unique
+userSchema.statics.isEmailUsed = async function (email) {
+  const user = await this.findOne({ email });
+  return !!user;
+};
+
+const User = mongoose.model("User", userSchema);
 export default User;
