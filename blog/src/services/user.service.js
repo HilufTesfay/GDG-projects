388a3186cb31c4c80c846d5f937ca2911f83cf9a
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-03-30 13:42:41 +0600

    signup implemented

--- blog/src/services/user.service.js
+++ blog/src/services/user.service.js
@@ -1,19 +1,22 @@
 import User from "../model/user.model.js";
+import CustomError from "../utils/custom.error.js";
 
 //This function signup user
 const signup = async (body) => {
   const { email } = body;
-  const is_eamil_taken = User.is_email_used(email);
-
+  const is_email_taken = User.is_email_used(email);
+  if (is_email_taken) {
+    throw new CustomError(400, "This Email is used");
+  }
   const user = await User.create(body);
   if (!user) {
-    console.log("unable to create user");
-    return;
+    throw new CustomError(400, "unable to create user");
   }
-  console.log("user created successfully");
-  return user;
+  return { message: "user created successfully", user: user };
 };
 
 // This function enable the user to signin
 const signin = async () => {};
 const signout = async () => {};
+
+export { signup };
