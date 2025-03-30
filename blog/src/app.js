046246702c108ae7525962c5480b24d081a06111
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-03-30 14:16:56 +0600

    bug fixed

--- blog/src/app.js
+++ blog/src/app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import API_route from "./routes/index.js";
+import {
+  convert_error,
+  handle_global_error,
+} from "./middleware/error.handler.js";
 
 const app = express();
 
@@ -6,15 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //api routes
-app.use("/api/v1", (req, res) => {
-  res.send("API v1 is working");
-});
+app.use("/api/v1", API_route);
 
 //error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+app.use(convert_error);
+app.use(handle_global_error);
 
 app.use("*", (req, res, next) => {
   res.status(404).send(`${req.originalUrl} not found`);
--- blog/src/utils/error.handler.js
+++ blog/src/utils/error.handler.js
@@ -1,7 +1,7 @@
 /*THis function changes function to middleware and handle any possible promise error 
  
 */
-const handle_promise_error = async (func) => (req, res, next) => {
+const handle_promise_error = (func) => (req, res, next) => {
   Promise.resolve(func(req, res, next)).catch((error) => next(error));
 };
 
