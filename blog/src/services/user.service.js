ae18a1ff86371283a329f8aa481fb5413a71f030
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-01 01:54:11 +0600

    signin implemented

--- blog/src/services/user.service.js
+++ blog/src/services/user.service.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import CustomError from "../utils/custom.error.js";
-
+import { generate_token } from "./token.service.js";
+import { DateTime } from "luxon";
 //This function signup user
 const signup = async (body) => {
   const { email } = body;
@@ -17,7 +18,19 @@ const signup = async (body) => {
 };
 
 // This function enable the user to signin
-const signin = async () => {};
+const signin = async (email, password) => {
+  const user = await User.findOne(email);
+  if (!user) {
+    throw new CustomError(403, "incorrect email or password");
+  }
+  const is_match = await user.comparePassword(password);
+  if (!is_match) {
+    throw new CustomError(403, "In correct email or password");
+  }
+  const expires = DateTime.now().plus({ minute: 5 });
+  const token = await generate_token(user.id, user.name, expires);
+  return { message: "login successfully", accessToken: token };
+};
 const signout = async () => {};
 
 export { signup };
