75a05bde25001667b720094386280c142badf708
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-01 03:25:07 +0600

    routes  protected by authentication

--- blog/src/routes/v1/blog.route.js
+++ blog/src/routes/v1/blog.route.js
@@ -5,8 +5,14 @@ import {
   edit_post,
   get_posts,
 } from "../../controller/blog.controller.js";
+import is_authenticated from "../../middleware/auth.js";
+
 const Router = expres.Router();
-Router.route("/").post(post_blog).get(get_posts).put(edit_post);
-Router.route("/:id").delete(delete_post);
+Router.route("/")
+  .post(is_authenticated, post_blog)
+  .get(is_authenticated, get_posts)
+  .put(is_authenticated, edit_post);
+
+Router.route("/:id").delete(is_authenticated, delete_post);
 
 export default Router;
